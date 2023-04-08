98a30ff94664e0c0c34cef188dfbad46d1b4df85
MichOchieng <MichOchieng@gmail.com>
2023-04-08 03:12:25 -0700

    First commit

---
+++ app/predict/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import Card from '../components/Card'
+
+const PredictPage = () => {
+  return (
+    <div className='page-container aid'>
+        <Card/>
+        <Card/>
+    </div>
+  )
+}
+
+export default PredictPage
