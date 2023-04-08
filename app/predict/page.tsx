fc78d7610394b84de3cec8ec55c1e34865dbfb89
MichOchieng <MichOchieng@gmail.com>
2023-04-08 03:45:27 -0700

    Added dummy cards

--- app/predict/page.tsx
+++ app/predict/page.tsx
@@ -4,7 +4,9 @@ import Card from '../components/Card'
 
 const PredictPage = () => {
   return (
-    <div className='page-container aid'>
+    <div className='page-container flex-row flex-wrap aid'>
+        <Card/>
+        <Card/>
         <Card/>
         <Card/>
     </div>
