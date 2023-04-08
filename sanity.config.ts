a313863cb7a928b58fea4c0beb1f7f7fe3fa689f
MichOchieng <MichOchieng@gmail.com>
2023-04-08 16:54:33 -0700

    Added vision tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
@@ -13,7 +14,7 @@ export default defineConfig({
   projectId,
   dataset,
 
-  plugins: [deskTool()],
+  plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
