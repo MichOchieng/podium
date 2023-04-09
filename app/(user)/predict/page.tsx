48573ff242f592f49c9d12ff0b9271d86ddc18f8
MichOchieng <MichOchieng@gmail.com>
2023-04-08 17:44:05 -0700

    Added sanity query and circuit list handler

--- app/(user)/predict/page.tsx
+++ app/(user)/predict/page.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import Card from '../../../components/Card'
+import { groq } from 'next-sanity'
+import { client } from '@/lib/sanity-client'
+import CircuitList from '@/components/CircuitList';
 
-const PredictPage = () => {
+
+const query = groq`
+*[_type=='circuit'] {
+  ...,
+} | order(name asc)
+`;
+
+async function PredictPage() {
+  const circuits = await client.fetch(query)
+
   return (
-    <div className='page-container flex-row flex-wrap aid'>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+    <div className='page-container aid'>
+      {/* Circuits */}
+      <CircuitList circuits={circuits} />
     </div>
   )
 }
