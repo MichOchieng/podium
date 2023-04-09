caef1e4a4ab71598ebc4a9f14ede23af3806b6a5
MichOchieng <MichOchieng@gmail.com>
2023-04-08 19:36:24 -0700

    Handles dynamic routing from card components

---
+++ components/ClientSideRoute.tsx
@@ -0,0 +1,18 @@
+'use client';
+{/* Allows dynamic routing from the card component*/}
+import Link from 'next/link'
+import React from 'react'
+
+const ClientSideRoute = ({
+    children, route, className
+  }: {
+    children: React.ReactNode,
+    route: string,
+    className: string
+  }) => {
+  return (
+    <Link href={route} className={className}>{children}</Link>
+  )
+}
+
+export default ClientSideRoute
