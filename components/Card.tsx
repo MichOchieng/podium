893c0551b4f4fac79820fc1e9f53225722121f9a
MichOchieng <MichOchieng@gmail.com>
2023-04-08 18:41:25 -0700

    Added Images and country name

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,6 @@
+import urlFor from '@/lib/urlFor'
 import { Circuit } from '@/typings'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -9,13 +11,13 @@ interface Props {
 const Card = ({ circuit }: Props) => {
   return (
     <div className='card'>
-      {/* Image */}
       {/* `/predict/${circuit.slug}` */}
-      <Link href={'/'}>
-
+      <Link className='flex items-center justify-center h-full w-full relative group' href={'/'}>
+        {/* title */}
+        <h1 className="font-formulaBold text-3xl absolute z-20 hover:scale-105 smoothed">{circuit.name}</h1>
+        <span className="bg-black/40 group-hover:bg-black/25 smoothed absolute z-10 h-full w-full flex"></span>
+        <Image className='object-cover h-full w-full z-0' fill src={urlFor(circuit.trackThumbnail).url()} alt='trackImg' />
       </Link>
-      {/* title */}
-      <h1 className="font-formulaBold text-2xl absolute hover:scale-105 smoothed">{circuit.name}</h1>
     </div>
   )
 }
