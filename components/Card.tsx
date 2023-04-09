42009e9d620660d849728cc459cb9cb7ab0b4d00
MichOchieng <MichOchieng@gmail.com>
2023-04-08 17:43:02 -0700

    Added circuit data

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,10 +1,21 @@
-'use client'
+import { Circuit } from '@/typings'
+import Link from 'next/link'
 import React from 'react'
 
-const Card = () => {
+interface Props {
+  circuit: Circuit
+}
+
+const Card = ({ circuit }: Props) => {
   return (
     <div className='card'>
-        tests
+      {/* Image */}
+      {/* `/predict/${circuit.slug}` */}
+      <Link href={'/'}>
+
+      </Link>
+      {/* title */}
+      <h1 className="font-formulaBold text-2xl absolute hover:scale-105 smoothed">{circuit.name}</h1>
     </div>
   )
 }
