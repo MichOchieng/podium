244f3a7e9aa8d4aa71a1620d945056104023a1fa
MichOchieng <MichOchieng@gmail.com>
2023-04-11 16:08:21 -0700

    Added about page

--- app/(user)/about/page.tsx
+++ app/(user)/about/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className='text-center'>
+      <p>
+      This project aims to predict whether a driver in a Formula 1 race will finish in the top 3 at a specific circuit by using a decision tree classifier model. The model uses telemetry data transmitted between a team and their car, with the driver&apos;s average lap time for the circuit as one of the key features. The dataset is gathered using Ergast&apos;s API and includes data from the constructors, results, races, and circuit tables available in the Ergast database.
+      </p>
+    </div>
   )
 }
 
