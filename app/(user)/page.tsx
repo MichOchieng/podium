faf2155f3948469959868258c9d83fe5bdf2bbe7
MichOchieng <MichOchieng@gmail.com>
2023-04-11 16:02:50 -0700

    Added nav options

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="page-container aid">
-      goto predictions
-      about the app
+    <main className="page-container h-[400px]">
+        {/* Circuits */}
+        <Link className="flex items-center justify-center w-64 h-28 my-5" href={'/predict'}>
+          <h1 className="mainButton">View Circuits</h1>
+        </Link>
+        {/* About */}
+        <Link className="flex items-center justify-center w-64 h-28 my-5" href={'/about'}>
+          <h1 className="mainButton">About Podium</h1>
+        </Link>
     </main>
   )
 }
