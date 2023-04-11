815e8c010e7b24dddc2ab15bee124d819011b49f
MichOchieng <MichOchieng@gmail.com>
2023-04-11 16:02:31 -0700

    Added timeout to result and fixed inline if

--- components/Results.tsx
+++ components/Results.tsx
@@ -4,11 +4,26 @@ interface Props {
     circuitName: string
 }
 
-const Results = ({ circuitName }: Props) => {
-    const [data, setData] = React.useState(null)
-    const [time, setTime] = React.useState('')
+type ResponseData = {
+    podium: boolean | null
+}
 
-    const handleSubmit = async (event:any) => {
+const Results = ({ circuitName }: Props) => {
+    const [data, setData]: any = React.useState(null)
+    const [time, setTime] = React.useState('')
+    const [showResult,setShowResult] = React.useState(false)
+
+    // Displays result for 5 seconds
+    React.useEffect(() => {
+        if (showResult) {
+          const timer = setTimeout(() => {
+            setShowResult(false);
+          }, 5000);
+          return () => clearTimeout(timer);
+        }
+      }, [showResult]);
+
+    const handleSubmit = async (event: any) => {
         event.preventDefault();
 
         const formData = {
@@ -23,8 +38,8 @@ const Results = ({ circuitName }: Props) => {
         };
 
         const response = await fetch('https://podium-model.herokuapp.com/predict', requestOptions);
-        const data = await response.json();
-
+        const data: ResponseData = await response.json();
+        setShowResult(true)
         setData(data);
     }
 
@@ -41,13 +56,21 @@ const Results = ({ circuitName }: Props) => {
                 <input className="bg-[#e10600] rounded-md h-11 w-24 mx-auto cursor-pointer" type="submit" value="Submit" />
             </form>
             {/* Prediction response */}
-            <div className="w-full flex items-center justify-center mt-5 aid">
+            <div className="w-full flex items-center justify-center mt-5 text-center p-5 smoothed overflow-hidden">
                 {
-                    data &&
-                    <>
-                    {console.log(data)}
-                        <h1 className='aid w-10 h-10'>test</h1>
-                    </>
+                    (data !== null) && // API Returned a result from the model\
+                        data.podium ? // Model returned True
+                        (
+                            <div className="">
+                                {showResult && <h1 className="smoothed">Congrats! Your time of {time} will land you on the Podium!</h1>}
+                            </div>
+                        )
+                        :
+                        (
+                            <div className="">
+                                {showResult && <h1 className="smoothed">Too slow! With a time of {time} you wont make it to the Podium!</h1>}
+                            </div>
+                        )
                 }
             </div>
         </>
